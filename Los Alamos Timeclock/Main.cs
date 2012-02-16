cc6f55c0a90c8e642db4ce50cf67e7b853c1c7b6
ajdepersio <ajdepersio@gmail.com>
2012-02-16 11:35:37 -0500

    Added ability to change database connection settings in case of failed connection

--- Los Alamos Timeclock/Main.cs
+++ Los Alamos Timeclock/Main.cs
@@ -11,7 +11,7 @@ using MySql.Data.MySqlClient;
 
 namespace Los_Alamos_Timeclock
 {
-    
+
     public partial class Main : Form
     {
         public static string week;
@@ -19,10 +19,10 @@ namespace Los_Alamos_Timeclock
         public static string status;
         public static string ID;
         public static string EName;
-        public static MySqlConnection myConnection= new MySqlConnection();
+        public static MySqlConnection myConnection = new MySqlConnection();
         public static MySqlDataReader reader;
         public static DateTime w;
-        public static string permissions = "";  
+        public static string permissions = "";
 
 
         public Main()
@@ -39,8 +39,8 @@ namespace Los_Alamos_Timeclock
             }
             w = w.Date;
             week = w.Date.ToShortDateString();
-            
-            
+
+
             panel1.Controls.Clear();
             panel1.Controls.Add(new Login());
             panel1.Controls[0].Dock = DockStyle.Fill;
@@ -51,26 +51,63 @@ namespace Los_Alamos_Timeclock
                 ";UID=" + Properties.Settings.Default.User + ";" +
                 ";PASSWORD=" + Properties.Settings.Default.Password + ";");
 
-            
+            connectDB(myConnection);
+        }
+
+        /* Method to connect to the database 
+         * @author Nate Rush
+         * @author Andrew DePersio ajdepersio@gmail.com
+         * 
+         * @param myConnection = the MySqlConnection server to be connected to
+         */
+        public void connectDB(MySqlConnection myConnection)
+        {
             try
             {
                 myConnection.Open();
                 myConnection.Close();
             }
+
+           //If connection fails, give user the option to reenter settings or exit the program
             catch (Exception f)
             {
-                MessageBox.Show("Failed to connect to database, make sure apache is running or change settings");
+                DialogResult result = MessageBox.Show("Failed to connect to database, make sure apache is running or change settings. \n Do you Want to change connection settings?",
+                    "Database Error", MessageBoxButtons.YesNo);
 
-                changeconnection(); //allows changing of database info, method not done yet
+                if (result == DialogResult.Yes)
+                {
+                    changeConnection();
+                }
+                else
+                {
+                    Environment.Exit(0);
+                }
             }
-            
         }
 
 
-        public void changeconnection()
+        /** Method to change database connection settings in case of connection error
+         * @author Andrew DePersio ajdepersio@gmail.com
+         */
+        public void changeConnection()
         {
-            
-            
+            //Get input from user
+            string server = Microsoft.VisualBasic.Interaction.InputBox("Enter Database IP", "", Properties.Settings.Default.IP, 0, 0);
+            string port = Microsoft.VisualBasic.Interaction.InputBox("Enter Database Port", "", Properties.Settings.Default.Port, 0, 0);
+            string database = Microsoft.VisualBasic.Interaction.InputBox("Enter Database Name", "", Properties.Settings.Default.Database, 0, 0);
+            string uid = Microsoft.VisualBasic.Interaction.InputBox("Enter Database User", "", Properties.Settings.Default.User, 0, 0);
+            string password = Microsoft.VisualBasic.Interaction.InputBox("Enter Database User Password", "", Properties.Settings.Default.Password, 0, 0);
+
+            //Build new sql connection
+            Main.myConnection = new MySqlConnection(
+                "SERVER=" + server +
+                ";PORT=" + port +
+                ";DATABASE=" + database +
+                ";UID=" + uid + ";" +
+                ";PASSWORD=" + password + ";");
+
+            //try to connect
+            connectDB(myConnection);
         }
 
 
@@ -108,10 +145,10 @@ namespace Los_Alamos_Timeclock
         {
             menu1.timer1.Start();
         }
-        
+
         private void loginControl1_Load(object sender, EventArgs e)
         {
-          
+
         }
 
         private void panel1_Paint(object sender, PaintEventArgs e)
