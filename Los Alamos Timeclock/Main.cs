8e3dcf678066d9450e10f5ba10d19ab8800d25d9
Nate Rush <datahunter11@gmail.com>
2012-04-14 22:59:06 -0400

    Changed the timeout back to 2 min

--- Los Alamos Timeclock/Main.cs
+++ Los Alamos Timeclock/Main.cs
@@ -466,7 +466,7 @@ namespace Los_Alamos_Timeclock
         //methods to deal with the timeout timer
 
         //ammount of time to wait before timing out
-        private TimeSpan timeoutTimelimit = TimeSpan.FromMinutes(30);
+        private TimeSpan timeoutTimelimit = TimeSpan.FromMinutes(2);
         private DateTime timerCompareTime = DateTime.Now;
         
         //start the timer
