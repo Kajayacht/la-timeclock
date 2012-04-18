b4ddb557ecd6dcd0c140861d963fb519e3522e72
ajdepersio <ajdepersio@gmail.com>
2012-04-17 20:49:14 -0400

    Added comments/cleaned up EditJobs.cs

--- Los Alamos Timeclock/Manager/Admin/EditJobs.cs
+++ Los Alamos Timeclock/Manager/Admin/EditJobs.cs
@@ -9,29 +9,33 @@ using System.Windows.Forms;
 
 namespace Los_Alamos_Timeclock.Manager.Admin
 {
+    /*
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+    /* Class to handle the creating, changing, and deleting of different jobs in the database */
+
     public partial class EditJobs : UserControl
     {
-
-            /*
-    This program is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
-
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-     */
-
         String filename = "";
+     
+        /* Initialization of the class */
         public EditJobs()
         {
             InitializeComponent();
 
+            //Load the background image
             try
             {
                 this.BackgroundImage = Image.FromFile(Properties.Settings.Default.backgroundImage);
@@ -41,6 +45,7 @@ namespace Los_Alamos_Timeclock.Manager.Admin
                 this.BackgroundImage = Properties.Resources._1287421014661;
             }
 
+            //Event handlers to handle the idle logout function
             this.KeyDown += new System.Windows.Forms.KeyEventHandler(Main.maininstance.notIdle_event);
             this.MouseMove += new System.Windows.Forms.MouseEventHandler(Main.maininstance.notIdle_event);
             jobsBox.MouseMove += new System.Windows.Forms.MouseEventHandler(Main.maininstance.notIdle_event);
@@ -55,17 +60,19 @@ namespace Los_Alamos_Timeclock.Manager.Admin
             jobsBox.DisplayMember = "getname";
             jobsBox.DataSource = Main.joblist;
 
-            //!Decimal.TryParse(pay.Text, out a)
         }
 
+        /* Method to get the information of the currently selected job */
         private void jobsBox_SelectedIndexChanged(object sender, EventArgs e)
         {
+            //populate the UI elements with the job info
             jobnameTextbox.Text = jobsBox.Text;
             jobsBox.ValueMember = "getpay";
             payChooser.Value = Decimal.Parse(jobsBox.SelectedValue.ToString());
             jobsBox.ValueMember = "getTipped";
             tippedBox.Checked = Boolean.Parse(jobsBox.SelectedValue.ToString());
 
+            //Get the information from the database
             try
             {
                 Main.myConnection.Open();
@@ -84,14 +91,17 @@ namespace Los_Alamos_Timeclock.Manager.Admin
             }
         }
 
+        /* Method to handle the validity of the job entered in */
         public Boolean validate()
         {
+            //Get the job information stored on the database
             Main.myConnection.Open();
             Main.maininstance.sqlReader("SELECT * FROM Jobs WHERE JID='"+jobnameTextbox.Text+"'");
             Boolean hasrows = Main.reader.HasRows;
             Main.reader.Close();
             Main.myConnection.Close();
 
+            //If there's already a job with that name in the database
             if (hasrows && jobnameTextbox.Text != jobsBox.Text)
             {
                 MessageBox.Show("A job with the name of " + jobnameTextbox.Text + " already exists");
@@ -103,6 +113,7 @@ namespace Los_Alamos_Timeclock.Manager.Admin
             }
         }
 
+        /* Method to reload the job info when the selected job is changed */
         public void refreshJobs()
         {
             Main.joblist = Main.maininstance.getJobs();
@@ -110,58 +121,77 @@ namespace Los_Alamos_Timeclock.Manager.Admin
             jobsBox.DisplayMember = "getname";
         }
 
+        /* Method to handle updating a job's information */
         private void updateJob_Click(object sender, EventArgs e)
         {
+            //if valid information is given
             if (validate())
             {
+                //Update the information in the database
                 Main.maininstance.sqlCommand("UPDATE Jobs SET JID='" + jobnameTextbox.Text.Replace(@"\", @"\\").Replace("'", @"\'") + "',JSPay='" + payChooser.Value + "',TippedJob='" + tippedBox.Checked + "', Filename='" + filename + "' WHERE JID='" + jobsBox.Text + "'");
                 MessageBox.Show("Update successful");
+                //Write change to the log file
                 Log.writeLog(Main.eName + " updated job: " + "\n Job= " + jobnameTextbox.Text.Replace(@"\", @"\\").Replace("'", @"\'") + "\n Starting Pay= " + payChooser.Value + "\n Tipped Job= " + tippedBox.Checked + "\n Image= " + filename);
                 refreshJobs();
             }
         }
 
+        /* Method to handle adding a new job to the database */
         private void newJob_Click(object sender, EventArgs e)
         {
             if (jobsBox.Text != jobnameTextbox.Text)
             {
+                //If valid information is given
                 if (validate())
                 {
+                    //Create the new job info on the database
                     MessageBox.Show("Insert successful");
                     Main.maininstance.sqlCommand("INSERT INTO Jobs Values('" + jobnameTextbox.Text.Replace(@"\", @"\\").Replace("'", @"\'") + "','" + payChooser.Value + "','" + tippedBox.Checked + "','" + filename + "')");
+                    //Write the change to the log file
                     Log.writeLog(Main.eName + " added job: " + "\n Job= " + jobnameTextbox.Text.Replace(@"\", @"\\").Replace("'", @"\'") + "\n Starting Pay= " + payChooser.Value + "\n Tipped Job= " + tippedBox.Checked);
                     refreshJobs();
                 }
             }
+            //If that job already exists
             else
             {
                 MessageBox.Show("A job with the name of " + jobnameTextbox.Text + " already exists");
             }
         }
 
+        /* Method to handle removing a job from the database */
         private void deleteJob_Click(object sender, EventArgs e)
         {
+            //Ask the user for confirmation
             DialogResult result = MessageBox.Show("Are you sure you want to delete this job? All "+jobsBox.Text+" shifts will also be removed",
                     "Delete Job?", MessageBoxButtons.YesNo);
 
+                //if yes
                 if (result == DialogResult.Yes)
                 {
+                    //remove the job from the database
                     Main.maininstance.sqlCommand("DELETE FROM Jobs WHERE JID='" + jobsBox.Text.Replace(@"\", @"\\").Replace("'", @"\'") + "'");
+                    //Write the change to the log file
                     Log.writeLog(Main.eName + " deleted job: " + "\n Job= " + jobnameTextbox.Text.Replace(@"\", @"\\").Replace("'", @"\'"));
                     refreshJobs();
                 }
         }
 
+        /* Method to handle selecting a job image for a job */
         private void selectImageButton_Click(object sender, EventArgs e)
         {
+            //Get the image file from the user
             OpenFileDialog imageDialog = new OpenFileDialog();
+            //Only show standard image files by default
             imageDialog.Filter = "Image Files(*.BMP;*.JPG;*.GIF;*.PNG)|*.BMP;*.JPG;*.GIF;*.PNG|All files (*.*)|*.*";
             imageDialog.FilterIndex = 1;
             imageDialog.InitialDirectory = Properties.Settings.Default.jobImageFolderPath + "\\images\\";
             imageDialog.RestoreDirectory = false;
 
+            //When the user selects an image
             if (imageDialog.ShowDialog() == DialogResult.OK)
             {
+                //Get the file information
                 filename = System.IO.Path.GetFileName(imageDialog.FileName);
                 String filepath = System.IO.Path.GetFullPath(imageDialog.FileName);
                 String directory = System.IO.Path.GetDirectoryName(imageDialog.FileName);
@@ -169,8 +199,9 @@ namespace Los_Alamos_Timeclock.Manager.Admin
                 //checks if the file is already in the directory
                 if (directory.ToLower() != (Properties.Settings.Default.jobImageFolderPath + "\\images").ToLower())
                 {
+                    //Copy the file to the images directory
                     try
-                    {
+                    {                        
                         System.IO.File.Copy(filepath, Properties.Settings.Default.jobImageFolderPath + "\\images\\" + filename);
                         filenameTextbox.Text = filename;
                         jobPicturebox.ImageLocation = Properties.Settings.Default.jobImageFolderPath + "\\images\\" + filename;
@@ -191,6 +222,7 @@ namespace Los_Alamos_Timeclock.Manager.Admin
                         }
                     }
                 }
+                //Set the image for that job
                 else
                 {
                     filenameTextbox.Text = filename;
